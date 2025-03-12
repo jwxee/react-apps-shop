c9f7c9153532db0de144d9b07e7f0225a8c80d82
GeRa <96356108+jwxee@users.noreply.github.com>
2025-03-12 21:31:49 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-apps-shop/", // Исправляет пути для GitHub Pages
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
+  base: "/react-apps-shop/", // Это исправляет пути для GitHub Pages
 });
